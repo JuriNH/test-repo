9e4596bf3d6ad54fa115bd9c34ba44558848d51d
JuriNH <jurand.maslowski@gmail.com>
2020-08-01 20:46:04 +0200

    Poprawione Adult2

--- adult.py
+++ adult.py
@@ -6,3 +6,5 @@ else:
     print("Do pełnoletności zostało", 18 - age, "lat")
 if age >=100 :
     print("Czy to naprawdę Twój wiek?")
+
+# można w taki sposób czy wszystkie IF powinny być pod sobą, a else po nich. ??
--- adult2.py
+++ adult2.py
@@ -3,14 +3,11 @@ age = int(input())
 print("Czy jesteś odpowiedzialny? T/N")
 responsible = input()
 
-if age > 100:
-    print("To naprawdę Twój wiek?")
-elif age >= 18 and responsible == "T":
-    print("Jesteś dorosłym człowiekiem")
+if age < 18:
+    print("Do pełnoletności brakuje Ci:", 18 - age, "lat")
+elif age >= 100:
+    print("Czy napewno masz tyle lat?")
 elif responsible != 'T':
     print("Popracuj nad odpowiedzialnością")
 else:
-    print("Do pełnoletności brakuje Ci:", 18 - age, "lat")
-
-## czemu nie działa? jesli dam 12 i N printuje "popracuj nad odpowiedzialnoscia",
-# zamiast "do pelnoletnosci brakuje..."
+    print("Jesteś dorosłym, odpowiedzialnym człowiekiem")
