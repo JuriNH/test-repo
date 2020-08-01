d845cdc8a6ff7a6b4e7f3c2e8c40af101b143769
JuriNH <jurand.maslowski@gmail.com>
2020-08-01 21:09:30 +0200

    debilo odporne

--- adult2.py
+++ adult2.py
@@ -7,7 +7,9 @@ if age < 18:
     print("Do pełnoletności brakuje Ci:", 18 - age, "lat")
 elif age >= 100:
     print("Czy napewno masz tyle lat?")
-elif responsible != 'T':
+elif responsible == 'N':
     print("Popracuj nad odpowiedzialnością")
-else:
+elif responsible == 'T':
     print("Jesteś dorosłym, odpowiedzialnym człowiekiem")
+else:
+    print("chyba cię powaliło, error bla bla bla")
