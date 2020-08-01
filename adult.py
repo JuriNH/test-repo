806babd2b10089a9f220815c84e880c86f3e00c6
JuriNH <jurand.maslowski@gmail.com>
2020-08-01 18:28:30 +0200

    kolejność IF

---
+++ adult.py
@@ -0,0 +1,8 @@
+print("Ile masz lat?")
+age = int(input())
+if age >=18 :
+    print("Jesteś dorosłym człowiekiem")
+else:
+    print("Do pełnoletności zostało", 18 - age, "lat")
+if age >=100 :
+    print("Czy to naprawdę Twój wiek?")
